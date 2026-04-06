248cb338bbb7fbcff95db20801894591ca262f62
Blake Coppens <147224985+BlakeCoppens@users.noreply.github.com>
2026-04-06 18:56:21 -0400

    Added reconstruction
    Commit #3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def readinput():
     K = int(input().strip())
     values = {}
-    for i in range(K):
+    for _ in range(K):
         key, value = input().split()
         values[key] = int(value)
         
@@ -13,10 +13,11 @@ def dynamic_programming(values, A, B):
     rows = len(A) + 1
     cols = len(B) + 1
     
-    dp = [[0 for i in range(cols)] for j in range(rows)]
+    dp = [[0 for i in range(cols)] for j in range(rows)] # 2d table for manipulation
     
     for i in range(1, rows):
         for j in range(1, cols):
+            #goes through each cell in the table, 
             if A[i-1] == B[j-1]:
                 # chooses best value at i j by taking the max between skipping A, skipping B, or matching characters and adding
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1],dp[i-1][j-1]+values[A[i-1]])
@@ -24,20 +25,33 @@ def dynamic_programming(values, A, B):
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
     return dp 
 
+def reconstruction(dp, values, A, B):
+    i = len(A)
+    j = len(B)
+    result = []
+    # start from bottom-right of DP table
+    while i > 0 and j > 0:
+        
+        # checks if the characters match and were used to find the optimal
+        if A[i-1] == B[j-1] and dp[i][j] == dp[i-1][j-1] + values[A[i-1]]:
+            result.append(A[i-1])
+            i -= 1
+            j -= 1
+        #checks if value came from skipping A
+        elif dp[i][j] == dp[i-1][j]:
+            i -= 1
+        #skips b
+        else:
+            j -= 1
+    return ''.join(reversed(result))
+
 def main():
     values, A, B = readinput()
-    #print(values)
-    #print(A)
-    #print(B)
     dp = dynamic_programming(values, A, B)
-    
-    #prints the dp table VVV
-    for row in dp:
-        print(row)
-        
-        
-    #prints max value VVV    
+    answer = reconstruction(dp, values, A, B)
     print(dp[len(A)][len(B)])
+    print(answer)
+    
     
     
 if __name__ == "__main__":
