39c214f89c2a8ce98c4d36886a6593cb5d7f5903
Blake Coppens <147224985+BlakeCoppens@users.noreply.github.com>
2026-04-06 18:53:10 -0400

    Added input logic
    Contribution #1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#blakey poo 
+def readinput():
+    K = int(input().strip())
+    values = {}
+    for i in range(K):
+        key, value = input().split()
+        values[key] = int(value)
+        
+    A = input().strip()
+    B = input().strip()
+    return values, A, B
+
+
+
+def main():
+    values, A, B = readinput()
+    print(values)
+    print(A)
+    print(B)
+    
+if __name__ == "__main__":
+    main()
